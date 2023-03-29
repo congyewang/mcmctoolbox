1aae8fd876ce3d58b62830131644e4b83fdbee59
Congye Wang <w-cy@outlook.com>
2023-03-29 17:51:07 +0100

    Add Q-invariant MALA to MCMC toolbox in version 0.11

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name='mcmctoolbox',
-    version='0.10.2',
-    description='MH, HMC, MALA, tMALA, tMALA/c, KSD',
+    version='0.11',
+    description='MH, HMC, MALA, tMALA, tMALA/c, Q-invariant MALA, KSD',
     url='https://github.com/congyewang/mcmctoolbox',
     author='Congye Wang',
     license='GPLv3+',
