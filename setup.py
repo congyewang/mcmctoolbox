60e5fab899ed1e24152721b87f35b5fe998c62a4
Congye Wang <w.congye2@newcastle.ac.uk>
2023-10-24 17:49:39 +0100

    Add AM & Fisher Adaptive MALA

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name='mcmctoolbox',
-    version='0.13.0',
-    description='MH, HMC, MALA, tMALA, tMALA/c, SA, KSD',
+    version='0.14.0',
+    description='MH, HMC, MALA, tMALA, tMALA/c, SA, Fisher Adaptive MALA, AM, KSD',
     url='https://github.com/congyewang/mcmctoolbox',
     author='Congye Wang',
     license='GPLv3+',
